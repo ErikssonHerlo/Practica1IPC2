140aa02a1f6b88792ce0c145e52efe1ef6d2341f
Eriksson Hernandez <erikssonhernandez201830459@cunoc.edu.gt>
2019-07-29 11:02:53 -0600

    Interfaz Grafica y estructura basica

---
+++ Library/src/library/Library.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import library.Interfaz.Inicio;
+import library.Interfaz.LibrosDisponibles;
+
+/**
+ *
+ * @author zofia
+ */
+public class Library {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+        Inicio Iniciar = new Inicio();
+        Iniciar.setVisible(true);
+          
+        // TODO code application logic here
+    }
+    
+}
